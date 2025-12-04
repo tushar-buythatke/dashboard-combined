179a5ccfcb5b2a50e332a99b553154b40553a30b
tushar-buythatke <tushar@buyhatke.com>
2025-12-04 12:08:53 +0530

    removed double entendre of logins

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from '@/contexts/AuthContext'
-import LoginPage from '@/pages/Login'
 import AnalyticsPage from '@/pages/Analytics'
-import { ProtectedRoute } from '@/components/ProtectedRoute'
 import { ThemeProvider } from '@/components/theme/theme-provider'
 import { CursorGlow, CursorRipple } from '@/components/ui/animated-background'
 
@@ -10,25 +7,15 @@ function App() {
   return (
     <ThemeProvider>
       <Router>
-        <AuthProvider>
-          {/* Global cursor effects */}
-          <CursorGlow />
-          <CursorRipple />
-          
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route 
-              path="/analytics" 
-              element={
-                <ProtectedRoute>
-                  <AnalyticsPage />
-                </ProtectedRoute>
-              } 
-            />
-            <Route path="/" element={<Navigate to="/analytics" replace />} />
-            <Route path="*" element={<Navigate to="/analytics" replace />} />
-          </Routes>
-        </AuthProvider>
+        {/* Global cursor effects */}
+        <CursorGlow />
+        <CursorRipple />
+        
+        <Routes>
+          <Route path="/analytics" element={<AnalyticsPage />} />
+          <Route path="/" element={<Navigate to="/analytics" replace />} />
+          <Route path="*" element={<Navigate to="/analytics" replace />} />
+        </Routes>
       </Router>
     </ThemeProvider>
   )
